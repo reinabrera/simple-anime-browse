d001a59209c474e841330830e2d98fa75e409ef7
Rein <rein@offshorly.com>
2024-02-27 18:44:37 +0800

    updated port

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import bodyParser from "body-parser";
 import 'dotenv/config';
 
 const app = express();
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 const API_URL = "https://api.jikan.moe/v4/"
 const d = new Date();
 const year = d.getFullYear()-1;
